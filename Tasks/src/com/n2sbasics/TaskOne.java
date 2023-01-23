8a4ec45c50f1d4a65e328965935284b9521b154f
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-01-22 21:45:49 -0500

    phase one

--- Tasks/src/com/n2sbasics/TaskOne.java
+++ Tasks/src/com/n2sbasics/TaskOne.java
@@ -1,6 +1,10 @@
 package com.n2sbasics;
 
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.ArrayList;
 
 public class TaskOne {
 
@@ -12,6 +16,7 @@ public class TaskOne {
 		//grade();
 		//reverseNumber();
 		//factorial();
+		//enhancedFor();
 		
 		//Scanner sc = new Scanner(System.in);
 		//System.out.println("Enter A Number For Fibonacci Sequence: ");
@@ -20,9 +25,22 @@ public class TaskOne {
 		//System.out.println(result);
 		
 		//error();
-		reverseString();
+		//reverseString();
+		//palindrome();
 		
+		int[] elements = {0,1,2,3,4,5,6,7,8,9,10};
+		System.out.println("The Duplicates Are: " + duplicates(elements));
+		//guessingGame();
 		
+		//char[] arr = new char[5];
+		//arr[0] = 'a';
+		//arr[1] = 'b';
+		//arr[2] = 'c';
+		//arr[3] = 'd';
+		//arr[4] = 'e';
+		//System.out.println(count(arr));
+		
+		//fizzbuzz();
 	}
 	
 	// Write a java program to print a person first name, last name, phone number, address and age. 
@@ -141,14 +159,12 @@ public class TaskOne {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter A Number You Would Like To Reverse: ");
 		int number = sc.nextInt();
-		int numberLength = (int)(Math.log10(number) + 1);
 		int newNumber = 0;
 		
-		for (int i = numberLength - 1; i >= 0; i--) {
+		while (number != 0) {
 			int temp = number % 10;
 			newNumber = newNumber * 10 + temp;
 			number = number / 10;
-			
 		}
 		System.out.println(newNumber);
 	}
@@ -163,6 +179,17 @@ public class TaskOne {
 			result = result * i;
 		}
 		System.out.print("The Factorial of " + number + " is " + result);
+	}
+	
+	public static void enhancedFor() {
+		Scanner sc = new Scanner(System.in);
+		int[] number = {1,2,3,4,5,6,7,8,9, 10}; 
+		int sum = 0;
+		
+		for (int count: number) {
+			sum += count;
+		}
+		System.out.println("The Total Sum Is " + sum);
 	}
 	
 	public static int fibonacci(int number) {
@@ -204,6 +231,87 @@ public class TaskOne {
 			newString += temp;
 			}
 		System.out.println(newString);
+	}
+	
+	public static void palindrome() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter A Number To Check For Palindrome Number: ");
+		int number = sc.nextInt();
+		int numberLength = (int)Math.log10(number) + 1;
+		int result = 0;
+		int tempNumber = number;
+		
+		for (int i = numberLength - 1; i >= 0; i--) {
+			int temp = tempNumber % 10;
+			result = result * 10 + temp;
+			tempNumber = tempNumber / 10;
+		}
+		System.out.println(number);
+		System.out.println(result);
+		if (number == result) {
+			System.out.print("Palindrome Number");
+		}
+		else {
+			System.out.print("Not A Palindrome Number");
+		}
+	}
+	
+	public static ArrayList<Integer> duplicates(int[] numbers) {
+		Set<Integer> number = new HashSet<Integer>();
+		ArrayList<Integer> arr = new ArrayList<Integer>(); 
+		
+		for (int i: numbers) {
+			if (number.contains(i)) {
+				arr.add(i);
+			}
+			number.add(i);
+		}
+		return arr;
+	}
+	
+	public static void guessingGame() {
+		Random random = new Random();
+		int number = 1 + random.nextInt(1000);
+		int guess;
+		int guesses = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Enter The Number (1 - 1000) To Guess Correctly: ");
+			guess = sc.nextInt();
+			guesses = guesses + 1;
+			
+			if (guess < number) {
+				System.out.println("Too Low. Try Higher!");
+			}
+			else {
+				System.out.println("Too High. Try Lower!");
+			}
+		}
+		while(number != guess);
+		
+		System.out.println("Finally You Guessed The Correct Number " + number);
+		System.out.println("It took you " + guesses + " guesses!");
+	}
+	
+	public static int count(char[] arr) {
+		return arr.length;
+	}
+	
+	public static void fizzbuzz() {
+		for(int i = 1; i < 100; i++) {
+			if (i % 3 == 0 && i % 5 != 0) {
+				System.out.println("Fizz");
+			}
+			else if (i % 5 == 0 && i % 3 != 0) {
+				System.out.println("Buzz");
+			}
+			else if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("Fizz Buzz");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
 	}
 	
 }
