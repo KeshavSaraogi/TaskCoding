65be1b2bf109b0a0b337b5cde3f4a40017c8b4e9
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-01-23 14:44:25 -0500

    vehicle

--- Tasks/src/vehicle/Driver.java
+++ Tasks/src/vehicle/Driver.java
@@ -3,6 +3,27 @@ package vehicle;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
+		Vehicle vh = new Vehicle();
+		
+		vh.start();
+		vh.setSpeed(30);
+		vh.turn("Left", 30);
+		System.out.println(vh.getSpeed());
+		vh.increaseSpeed(100);
+		vh.decreaseSpeed(40);
+		vh.stop();
+		
+		System.out.println("##########");
+		
+		Car car = new Car();
+		car.start();
+		car.setSpeed(80);
+		car.openTrunk();
+		
+		System.out.println("##########");
+		
+		Truck truck = new Truck();
+		truck.openTailgate();
+		
 	}
 }
